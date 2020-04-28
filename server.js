2fd220239a5a3ef52b33301b6aac05d12dd361f4
Koen Schipper <koen.schipper@me.com>
2020-04-28 17:27:45 +0200

    update server.js

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+console.log(process.env.NODE_ENV);
+
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
